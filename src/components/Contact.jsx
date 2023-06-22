519872dac4bd4e6758e2bb6de392f7256ede6e14
Ben McIntyre <ben@Bens-MBP.fios-router.home>
2023-06-22 09:47:11 -0700

    Updates to main

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,6 +26,10 @@ const Contact = () => {
       .then((result) => {
         console.log(result.text);
         setIsSubmitted(true);
+        setTimeout(() => {
+          formRef.current.reset();
+          setIsSubmitted(false);
+        }, 1250);
       })
       .catch((error) => {
         console.error(error.text);
@@ -33,63 +37,35 @@ const Contact = () => {
   };
 
   return (
-    <div className={`${styles.container} ${isLoaded ? styles.visible : ''} ${isSubmitted ? styles.hidden : ''}`}>
-      {!isSubmitted ? (
-        <form ref={formRef} onSubmit={handleSubmit} className={styles.fadeInOut}>
-          <div className={styles['form-item']}>
-            <label htmlFor="name" className={styles['form-label']}>
-              Name:
-            </label>
-            <br />
-            <input className={styles['form-text']} type="text" id="name" name="name" required maxLength="40" pattern="[A-Za-z ]+" />
-          </div>
-          <div className={styles['form-item']}>
-            <label htmlFor="email" className={styles['form-label']}>
-              Email:
-            </label>
-            <br />
-            <input className={styles['form-text']} type="email" id="email" name="email" required />
-          </div>
-          <div className={styles['form-item']}>
-            <label htmlFor="subject" className={styles['form-label']}>
-              Subject:
-            </label>
-            <br />
-            <input className={styles['form-text']} type="text" id="subject" name="subject" required maxLength="50" />
-          </div>
-          <div className={styles['form-item']}>
-            <label htmlFor="message" className={styles['form-label']}>
-              Message:
-            </label>
-            <br />
-            <textarea className={`${styles['form-text']} ${styles['message-input-box']}`} id="message" name="message" required maxLength="500"></textarea>
-          </div>
-          <button type="submit" className={styles['submit-button']}>
-            Submit
-          </button>
-        </form>
-      ) : (
-        <p className={`${styles['success-message']} ${styles.visible} ${styles.fadeInOut}`}>
-          Form submission successful!
-        </p>
-      )}
+    <div className={`${styles.container} ${isLoaded ? styles.visible : ''} ${isSubmitted ? styles.faded : ''}`}>
+      <form ref={formRef} onSubmit={handleSubmit} className={isSubmitted ? styles.hidden : ''}>
+        <div className={styles['form-item']}>
+          <label htmlFor="name" className={styles['form-label']}>Name:</label>
+          <br />
+          <input className={styles['form-text']} type="text" id="name" name="name" required maxLength="40" pattern="[A-Za-z ]+" />
+        </div>
+        <div className={styles['form-item']}>
+          <label htmlFor="email" className={styles['form-label']}>Email:</label>
+          <br />
+          <input className={styles['form-text']} type="email" id="email" name="email" required />
+        </div>
+        <div className={styles['form-item']}>
+          <label htmlFor="subject" className={styles['form-label']}>Subject:</label>
+          <br />
+          <input className={styles['form-text']} type="text" id="subject" name="subject" required maxLength="50" />
+        </div>
+        <div className={styles['form-item']}>
+          <label htmlFor="message" className={styles['form-label']}>Message:</label>
+          <br />
+          <textarea className={`${styles['form-text']} ${styles['message-input-box']}`} id="message" name="message" required maxLength="500"></textarea>
+        </div>
+        <button type="submit" className={styles['submit-button']}>Submit</button>
+      </form>
       <div className={styles['contact-info']}>
-        <a href="tel:+13306065612" className={styles['contact-link']}>
-          <FaPhoneSquareAlt className={styles['contact-icon']} />
-          (330) 606-5612
-        </a>
-        <a href="mailto:bendmcintyre@gmail.com" className={styles['contact-link']}>
-          <FaEnvelope className={styles['contact-icon']} />
-          bendmcintyre@gmail.com
-        </a>
-        <a href="https://www.linkedin.com/in/ben-mcintyre-profile/" target="_blank" rel="noopener noreferrer" className={styles['contact-link']}>
-          <FaLinkedin className={styles['contact-icon']} />
-          LinkedIn
-        </a>
-        <a href="https://github.com/bendmcintyre?tab=repositories" target="_blank" rel="noopener noreferrer" className={styles['contact-link']}>
-          <FaGithub className={styles['contact-icon']} />
-          GitHub
-        </a>
+        <a href="tel:+13306065612" className={styles['contact-link']}><FaPhoneSquareAlt className={styles['contact-icon']} /> (330) 606-5612</a>
+        <a href="mailto:bendmcintyre@gmail.com" className={styles['contact-link']}><FaEnvelope className={styles['contact-icon']} /> bendmcintyre@gmail.com</a>
+        <a href="https://www.linkedin.com/in/ben-mcintyre-profile/" target="_blank" rel="noopener noreferrer" className={styles['contact-link']}><FaLinkedin className={styles['contact-icon']} /> LinkedIn</a>
+        <a href="https://github.com/bendmcintyre?tab=repositories" target="_blank" rel="noopener noreferrer" className={styles['contact-link']}><FaGithub className={styles['contact-icon']} /> GitHub</a>
       </div>
     </div>
   );
